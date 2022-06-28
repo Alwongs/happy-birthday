e4fea45b05a4a68ae336e4356049026c1ca0636d
Alwong <alwong@ya.ru>
2022-06-28 21:40:02 +0400

    first init

---
+++ src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home-page"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+.home-page {
+    background-image: url('../assets/img/jpg/sea.jpg');
+    background-size: cover;
+    height: 100vh;
+}
+
+</style>
