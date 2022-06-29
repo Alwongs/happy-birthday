0044199d7b128a25298d383f7058d19aac5f5511
Alexander <alwong@ya.ru>
2022-06-29 11:38:59 +0400

    trying birthday

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,30 @@
 <template>
-    <div class="home-page"></div>
+    <div class="home-page">
+        <h1 :style="{ 'margin-right': gap + '%' }">Happy birthday</h1>
+        <footer :style="{ 'margin-right': gap2 + '%' }">to you!!!</footer>
+    </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            gap: -200,
+            gap2: -200
+        }
+    },
+    methods: {
+    move() {
+        setInterval(() => {
+            if(this.gap < 0) {
+                this.gap++;
+            }
+        }, 10)
+    },
+    },
+    mounted() {
+        this.move();
+    }
 }
 </script>
 
@@ -14,6 +34,12 @@ export default {
     background-image: url('../assets/img/jpg/sea.jpg');
     background-size: cover;
     height: 100vh;
+}
+h1 {
+    text-align: center;
+    padding: 32px;
+    font-size: 64px;
+    color: white;
 }
 
 </style>
